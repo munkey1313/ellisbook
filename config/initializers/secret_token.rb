888e7a9db5d0d18441e3095820338fcb44390b41
Randie Ellis <ellisfam1313@gmail.com>
2014-01-27 15:49:29 -0700

    Created a rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ellisbook::Application.config.secret_key_base = '74de04e2a985628346df3782825e89f7611f937d9a49fa863fa49523c59fb5d511194f07a8603db19f73e9252f980823935698084ee127d99a16cfa5a05fe8af'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ellisbook::Application.config.session_store :cookie_store, key: '_ellisbook_session'
